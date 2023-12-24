64b417850014ba830e38682703309fce5b789faf
fahimahammed <fahimahammed.cse@gmail.com>
2023-12-24 17:17:26 +0600

    update user status

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,5 +1,7 @@
 import { Doctor, UserRole } from "@prisma/client";
 import prisma from "../../../shared/prisma";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
 
 const createDoctor = async (doctor: Doctor, userData: any): Promise<Doctor> => {
     const result = await prisma.$transaction(async (transactionClient) => {
@@ -24,6 +26,28 @@ const createDoctor = async (doctor: Doctor, userData: any): Promise<Doctor> => {
     return result;
 };
 
+
+const changeProfileStatus = async (userId: string, status: any) => {
+    const isUserExist = await prisma.user.findUnique({
+        where: {
+            id: userId
+        }
+    });
+    if (!isUserExist) {
+        throw new ApiError(httpStatus.BAD_REQUEST, "User does not exists!")
+    }
+
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: userId
+        },
+        data: {
+            status
+        }
+    });
+
+    return updatedUser;
+}
 
 
 export const UserServices = {
