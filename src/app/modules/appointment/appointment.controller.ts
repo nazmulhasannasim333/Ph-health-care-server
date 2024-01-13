5944361e41ce206585abc537cf07f7511359cf7b
fahimahammed <fahimahammed.cse@gmail.com>
2024-01-13 18:22:32 +0600

    [update] appointment management

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -6,62 +6,6 @@ import { AppointmentServices } from './appointment.services';
 import { IAuthUser } from '../../../interfaces/common';
 import pick from '../../../shared/pick';
 
-// const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
-//     const filters = pick(req.query, adminFilterableFields);
-//     const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
-//     const result = await AdminService.getAllFromDB(filters, options);
-//     sendResponse(res, {
-//         statusCode: httpStatus.OK,
-//         success: true,
-//         message: 'Admin retrieval successfully',
-//         meta: result.meta,
-//         data: result.data,
-//     });
-// });
-
-// const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
-//     const { id } = req.params;
-//     const result = await AdminService.getByIdFromDB(id);
-//     sendResponse(res, {
-//         statusCode: httpStatus.OK,
-//         success: true,
-//         message: 'Admin retrieval successfully',
-//         data: result,
-//     });
-// });
-
-// const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
-//     const { id } = req.params;
-//     const result = await AdminService.updateIntoDB(id, req.body);
-//     sendResponse(res, {
-//         statusCode: httpStatus.OK,
-//         success: true,
-//         message: 'Admin updated successfully',
-//         data: result,
-//     });
-// });
-
-// const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
-//     const { id } = req.params;
-//     const result = await AdminService.deleteFromDB(id);
-//     sendResponse(res, {
-//         statusCode: httpStatus.OK,
-//         success: true,
-//         message: 'Admin deleted successfully',
-//         data: result,
-//     });
-// });
-// const softDelete = catchAsync(async (req: Request, res: Response) => {
-//     const { id } = req.params;
-//     const result = await AdminService.softDelete(id);
-//     sendResponse(res, {
-//         statusCode: httpStatus.OK,
-//         success: true,
-//         message: 'Admin soft deleted successfully',
-//         data: result,
-//     });
-// });
-
 const createAppointment = catchAsync(async (req: Request, res: Response) => {
     const user = req.user;
     const result = await AppointmentServices.createAppointment(req.body, user as IAuthUser);
