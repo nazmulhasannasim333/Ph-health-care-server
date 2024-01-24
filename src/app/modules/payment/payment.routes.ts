c5ab7d9f25cc270a63b3835cf06bb4e5eea0d282
fahimahammed <fahimahammed.cse@gmail.com>
2024-01-24 18:51:53 +0600

    role based access permission added

--- src/app/modules/payment/payment.routes.ts
+++ src/app/modules/payment/payment.routes.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { PaymentController } from './payment.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 router.post('/ipn', PaymentController.validate)
 
-router.post('/init/:appointmentId', PaymentController.initPayment)
+router.post(
+    '/init/:appointmentId',
+    auth(ENUM_USER_ROLE.PATIENT),
+    PaymentController.initPayment
+);
 
 
 
--- src/app/modules/schedule/schedule.route.ts
+++ src/app/modules/schedule/schedule.route.ts
@@ -2,18 +2,33 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { ScheduleValidation } from './schedule.validations';
 import { ScheduleController } from './schedule.controller';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
-router.get('/', ScheduleController.getAllFromDB);
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.PATIENT),
+  ScheduleController.getAllFromDB
+);
 
-router.get('/:id', ScheduleController.getByIdFromDB);
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.PATIENT),
+  ScheduleController.getByIdFromDB
+);
 
 // router.patch('/:id', PatientController.updateIntoDB);
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(ScheduleValidation.create),
   ScheduleController.insertIntoDB,
 );
-router.delete('/:id', ScheduleController.deleteFromDB);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  ScheduleController.deleteFromDB
+);
 
 export const ScheduleRoutes = router;
